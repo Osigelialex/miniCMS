1b956e0d81e13fdfebbb77d09a28471a9ce91874
Osigelialex <osigelialex@gmail.com>
2025-10-03 12:05:14 +0100

    switched to client engine for prisma

--- app/config/db.server.ts
+++ app/config/db.server.ts
@@ -1,13 +1,21 @@
 import { PrismaClient } from "../../prisma/generated/prisma/client";
-
-let prisma: PrismaClient;
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
   var __db: PrismaClient | undefined;
 }
 
+let prisma: PrismaClient;
+
 if (!global.__db) {
-  global.__db = new PrismaClient();
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  const adapter = new PrismaPg(pool);
+
+  global.__db = new PrismaClient({ adapter });
 }
 
 prisma = global.__db;
