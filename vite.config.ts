97f293be19ce6e361ee62a20b9238947749917e5
Osigelialex <osigelialex@gmail.com>
2025-10-03 11:31:51 +0100

    add vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  ssr: {
-    external: ["@prisma/client", ".prisma/client"],
-  },
-  optimizeDeps: {
-    exclude: ["@prisma/client"],
-  },
 });
